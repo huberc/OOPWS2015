0ce970c0ea3c1647c565193d203716c14a36973f
mlangows <e1426581@student.tuwien.ac.at>
2015-10-14 15:18:02 +0200

    basic framework

--- oop-ue/src/main/java/oop/SimulationResult.java
+++ oop-ue/src/main/java/oop/SimulationResult.java
@@ -2,4 +2,83 @@ package oop;
 
 public class SimulationResult {
 
+	/**
+	 * Total amount of living wood in the wood.
+	 */
+	private double totalLivingWood;
+
+	/**
+	 * Total amount of deadwood.
+	 */
+	private double totalDeadWood;
+
+	/**
+	 * The total amount of already harvested (but not yet processed) wood.
+	 */
+	private double totalHarvestedWood;
+
+	/**
+	 * The total amount of already processed wood.
+	 */
+	private double totalProcessedWood;
+
+	/**
+	 * Total amount of carbon dioxide bound by the wood at the current point in
+	 * time.
+	 * 
+	 * NOTE: Could be calculated in the <code>Result</code>, for ease of
+	 * understanding not done here in order to cantralize all calculations.
+	 */
+	private double totalBoundCO2;
+
+	/**
+	 * Formats the properties of this <code>SimulationResult</code> as an
+	 * ASCII-Table suitable for printing to a console.
+	 * 
+	 * @return a <code>String</code> holding a formatted table.
+	 */
+	public String createOutputTable() {
+		return null;
+	}
+
+	public double getTotalLivingWood() {
+		return totalLivingWood;
+	}
+
+	public void setTotalLivingWood(double totalLivingWood) {
+		this.totalLivingWood = totalLivingWood;
+	}
+
+	public double getTotalDeadWood() {
+		return totalDeadWood;
+	}
+
+	public void setTotalDeadWood(double totalDeadWood) {
+		this.totalDeadWood = totalDeadWood;
+	}
+
+	public double getTotalHarvestedWood() {
+		return totalHarvestedWood;
+	}
+
+	public void setTotalHarvestedWood(double totalHarvestedWood) {
+		this.totalHarvestedWood = totalHarvestedWood;
+	}
+
+	public double getTotalProcessedWood() {
+		return totalProcessedWood;
+	}
+
+	public void setTotalProcessedWood(double totalProcessedWood) {
+		this.totalProcessedWood = totalProcessedWood;
+	}
+
+	public double getTotalBoundCO2() {
+		return totalBoundCO2;
+	}
+
+	public void setTotalBoundCO2(double totalBoundCO2) {
+		this.totalBoundCO2 = totalBoundCO2;
+	}
+
 }
--- oop-ue/src/main/java/oop/Simulator.java
+++ oop-ue/src/main/java/oop/Simulator.java
@@ -2,7 +2,19 @@ package oop;
 
 public class Simulator {
 
-	public SimulationResult simulate(SimulationRequest req){
-		return null;
+	/**
+	 * Executes a simulation run based on a given <code>SimulationRequest</code>
+	 * for a given number of years.
+	 * 
+	 * @param req
+	 *            the request holding all simulation input params
+	 * @param years
+	 *            the number of years to simulate
+	 * @return a <code>SimulationResult</code> describing the state of the wood
+	 *         after <code>years</code> years
+	 */
+	public SimulationResult simulate(SimulationRequest req, int years) {
+		SimulationResult retVal = new SimulationResult();
+		return retVal;
 	}
 }
