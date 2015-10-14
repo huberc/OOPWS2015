e063ae7b9680d35b0e20babdf356ed0a19608165
mlangows <e1426581@student.tuwien.ac.at>
2015-10-14 15:02:54 +0200

    added simrequrest

---
+++ oop-ue/src/main/java/oop/SimulationRequest.java
@@ -0,0 +1,97 @@
+package oop;
+
+public class SimulationRequest {
+
+	/**
+	 * The amount of living wood at the start of a simulation.
+	 */
+	private double startLivingWood;
+
+	/**
+	 * The amount of daedwood at the start of a simulation.
+	 */
+	private double startDeadWood;
+
+	/**
+	 * Average wood growth per year
+	 */
+	private double avgWoodGrowth;
+
+	/**
+	 * Average amount of wood harvested per year.
+	 */
+	private double avgHarvestYearly;
+
+	/**
+	 * Average amount of dead wood which remains in the wood per year.
+	 */
+	private double avgDeadWoodYearly;
+
+	/**
+	 * Average yearly percentage of wood that is processed. Measured against the
+	 * total amount of living wood.
+	 */
+	private double avgProcessedWoodYearly;
+
+	/**
+	 * Average yearly amount of decompositioned wood. Measured against the total
+	 * amount of dead wood.
+	 */
+	private double avgDecompWoodYearly;
+
+	public double getStartLivingWood() {
+		return startLivingWood;
+	}
+
+	public void setStartLivingWood(double startLivingWood) {
+		this.startLivingWood = startLivingWood;
+	}
+
+	public double getStartDeadWood() {
+		return startDeadWood;
+	}
+
+	public void setStartDeadWood(double startDeadWood) {
+		this.startDeadWood = startDeadWood;
+	}
+
+	public double getAvgWoodGrowth() {
+		return avgWoodGrowth;
+	}
+
+	public void setAvgWoodGrowth(double avgWoodGrowth) {
+		this.avgWoodGrowth = avgWoodGrowth;
+	}
+
+	public double getAvgHarvestYearly() {
+		return avgHarvestYearly;
+	}
+
+	public void setAvgHarvestYearly(double avgHarvestYearly) {
+		this.avgHarvestYearly = avgHarvestYearly;
+	}
+
+	public double getAvgDeadWoodYearly() {
+		return avgDeadWoodYearly;
+	}
+
+	public void setAvgDeadWoodYearly(double avgDeadWoodYearly) {
+		this.avgDeadWoodYearly = avgDeadWoodYearly;
+	}
+
+	public double getAvgProcessedWoodYearly() {
+		return avgProcessedWoodYearly;
+	}
+
+	public void setAvgProcessedWoodYearly(double avgProcessedWoodYearly) {
+		this.avgProcessedWoodYearly = avgProcessedWoodYearly;
+	}
+
+	public double getAvgDecompWoodYearly() {
+		return avgDecompWoodYearly;
+	}
+
+	public void setAvgDecompWoodYearly(double avgDecompWoodYearly) {
+		this.avgDecompWoodYearly = avgDecompWoodYearly;
+	}
+}
