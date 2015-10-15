49385c82aa080825a2739e667eee579bb6b20aff
huberc <huberc6@gmail.com>
2015-10-15 12:17:33 +0200

    simulator erstellt

--- oop-ue/src/main/java/oop/Simulator.java
+++ oop-ue/src/main/java/oop/Simulator.java
@@ -1,5 +1,7 @@
 package oop;
 
+import oop.SimulationResult.SimulationRecord;
+
 public class Simulator {
 
 	/**
@@ -14,7 +16,58 @@ public class Simulator {
 	 *         after <code>years</code> years
 	 */
 	public SimulationResult simulate(SimulationRequest req, int years) {
-		SimulationResult retVal = new SimulationResult();
+
+
+		double totalLivingWoodpast = 0;
+		double totalDeadWoodpast = 0;
+		double totalHarvestedWoodpast = 0;
+		double totalProcessedWoodpast = 0;
+		double totalBoundCO2past = 0;
+
+		double startlivingWood = req.getStartLivingWood();
+		double startDeadWood = req.getStartDeadWood();
+		double avgHarvestYearly = req.getAvgHarvestYearly();
+		double avgWoodGrowth = req.getAvgWoodGrowth();
+
+		SimulationResult retVal = new SimulationResult(years);
+		SimulationRecord rec = retVal.new SimulationRecord();
+
+		for(int i=0;i<=years;i++){
+
+			if(i==0){
+
+				rec.setTotalLivingWood(req.getStartLivingWood());
+				totalLivingWoodpast = req.getStartLivingWood();
+
+				rec.setTotalDeadWood(req.getStartDeadWood());
+				totalDeadWoodpast = req.getStartDeadWood();
+
+				rec.setTotalHarvestedWood(0);
+
+				rec.setTotalProcessedWood(0);
+				totalProcessedWoodpast = 0;
+
+				rec.setTotalBoundCO2(req.getStartLivingWood() + req.getStartDeadWood());
+			}
+			else{
+				rec.setTotalLivingWood(totalLivingWoodpast + req.getAvgWoodGrowth() - req.getAvgHarvestYearly() - req.getAvgDeadWoodYearly());
+				totalLivingWoodpast = totalLivingWoodpast + req.getAvgWoodGrowth() - req.getAvgHarvestYearly() - req.getAvgDeadWoodYearly();
+
+				rec.setTotalDeadWood((totalDeadWoodpast + req.getAvgDeadWoodYearly()) *(1-req.getAvgDecompWoodYearly()));
+				totalDeadWoodpast = (totalDeadWoodpast + req.getAvgDeadWoodYearly()) *(1-req.getAvgDecompWoodYearly());
+
+				rec.setTotalHarvestedWood(req.getAvgHarvestYearly() * (1-req.getAvgProcessedWoodYearly()));
+				totalHarvestedWoodpast = req.getAvgHarvestYearly() * (1-req.getAvgProcessedWoodYearly());
+
+				rec.setTotalProcessedWood(totalProcessedWoodpast + (req.getAvgHarvestYearly()*req.getAvgProcessedWoodYearly()));
+				totalProcessedWoodpast = totalProcessedWoodpast + (req.getAvgHarvestYearly()*req.getAvgProcessedWoodYearly());
+
+				rec.setTotalBoundCO2(totalLivingWoodpast + totalDeadWoodpast + totalHarvestedWoodpast);
+			}
+
+			retVal.addSimulationRecordForYear(rec,i);
+		}
+
 		return retVal;
 	}
 }
