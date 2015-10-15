fb00a962d681ef4b4715f1d68f80c1ba2a6be47e
mlangows <e1426581@student.tuwien.ac.at>
2015-10-15 11:41:18 +0200

    added class for simulation record to have yearly snapshots

--- oop-ue/src/main/java/oop/SimulationResult.java
+++ oop-ue/src/main/java/oop/SimulationResult.java
@@ -1,45 +1,90 @@
 package oop;
 
+/**
+ * This class describes the result of a simulation. The state of the environment
+ * is stored in a snapshot for each simulated year, i.e. the result has one
+ * <code>SimulatedRecord</code> for each year, including year zero, i.e. the
+ * initial state
+ * 
+ * @author Michael Langowski, e1426581student.tuwien.ac.at
+ *
+ */
 public class SimulationResult {
 
+	/**
+	 * internal storage for yearly snapshots.
+	 */
 	private SimulationRecord[] dataPerYears;
 
 	/**
-	 * Creates a new <code>SimulationResult</code>
+	 * Creates a new <code>SimulationResult</code> with a pre-initialized
+	 * storage for <code>yearsSimulated + 1</code> years where the general
+	 * contract is that the "0-th" record holds the initial state.
 	 * 
 	 * @param yearsSimulated
+	 *            the number of years simulated
 	 */
 	public SimulationResult(int yearsSimulated) {
-		this.dataPerYears = new SimulationRecord[yearsSimulated];
+		this.dataPerYears = new SimulationRecord[yearsSimulated + 1];
 	}
 
+	/**
+	 * Adds a new simulation record (i.e. environment snapshot after the
+	 * <code>year</code>-th year) to this <code>SimulationResult</code>
+	 * 
+	 * @param rec
+	 *            the <code>SimulationRecord</code> to add
+	 * @param year
+	 *            the number of years elapsed in the simulation (where 0 would
+	 *            mean the initial state)
+	 */
 	public void addSimulationRecordForYear(SimulationRecord rec, int year) {
-
+		this.dataPerYears[year] = rec;
 	}
 
+	/**
+	 * Retrieves the <code>SimulationRecord</code> representing the environment
+	 * state after <code>year</code> years.
+	 * 
+	 * @param year
+	 *            the year in the simulation scenario that should be retrieved
+	 * @return the <code>SimulationRecord</code> representing the environment
+	 *         state after <code>year</code>
+	 */
+	public SimulationRecord getRecordForYear(int year) {
+		return this.dataPerYears[year];
+	}
+
+	/**
+	 * Formats the properties of this <code>SimulationResult</code> as an
+	 * ASCII-Table suitable for printing to a console.
+	 * 
+	 * @return a <code>String</code> holding formatted table.
+	 */
+	public String createOutputTable() {
+
+		String output;
+		int livingWoodspace = 15;
+		int deadWoodspace = 23;
+		int harvestedWoodspace = 16;
+		int processedWoddspace = 18;
+		int boundCO2space = 16;
+
+		output = "-----------------------------------------------------------------------------------------------------";
+		output += "| Jahr | lebendes Holz | verbleibendes Totholz | geerntets Holz | verwertetes Holz | gebundenes CO2 |";
+		output += "|  1   | 2,90908       | 0,89789               | 15,897234      | 18,7238409       | 2              |";
+		output += "-----------------------------------------------------------------------------------------------------";
+		return null;
+	}
+	
+	/**
+	 * A <code>SimulationRecord</code> represents the state of the simulated
+	 * environment at a specific point in time.
+	 * 
+	 * @author Michael Langowski, e142658@student.tuwien.ac.at
+	 *
+	 */
 	public class SimulationRecord {
-
-		/**
-		 * Formats the properties of this <code>SimulationResult</code> as an
-		 * ASCII-Table suitable for printing to a console.
-		 * 
-		 * @return a <code>String</code> holding formatted table.
-		 */
-		public String createOutputTable() {
-
-			String output;
-			int livingWoodspace = 15;
-			int deadWoodspace = 23;
-			int harvestedWoodspace = 16;
-			int processedWoddspace = 18;
-			int boundCO2space = 16;
-
-			output = "-----------------------------------------------------------------------------------------------------";
-			output += "| Jahr | lebendes Holz | verbleibendes Totholz | geerntets Holz | verwertetes Holz | gebundenes CO2 |";
-			output += "|  1   | 2,90908       | 0,89789               | 15,897234      | 18,7238409       | 2              |";
-			output += "-----------------------------------------------------------------------------------------------------";
-			return null;
-		}
 
 		/**
 		 * Total amount of living wood in the wood.
