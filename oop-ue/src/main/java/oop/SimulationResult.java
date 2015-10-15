919fb5389de0fb2f7f0363c946dae9b954d02c7a
mlangows <e1426581@student.tuwien.ac.at>
2015-10-15 10:50:15 +0200

    simulation result adapted

--- oop-ue/src/main/java/oop/SimulationResult.java
+++ oop-ue/src/main/java/oop/SimulationResult.java
@@ -2,95 +2,114 @@ package oop;
 
 public class SimulationResult {
 
-	/**
-	 * Total amount of living wood in the wood.
-	 */
-	private double totalLivingWood;
+	private SimulationRecord[] dataPerYears;
 
 	/**
-	 * Total amount of deadwood.
-	 */
-	private double totalDeadWood;
-
-	/**
-	 * The total amount of already harvested (but not yet processed) wood.
-	 */
-	private double totalHarvestedWood;
-
-	/**
-	 * The total amount of already processed wood.
-	 */
-	private double totalProcessedWood;
-
-	/**
-	 * Total amount of carbon dioxide bound by the wood at the current point in
-	 * time.
+	 * Creates a new <code>SimulationResult</code>
 	 * 
-	 * NOTE: Could be calculated in the <code>Result</code>, for ease of
-	 * understanding not done here in order to cantralize all calculations.
+	 * @param yearsSimulated
 	 */
-	private double totalBoundCO2;
-
-	/**
-	 * Formats the properties of this <code>SimulationResult</code> as an
-	 * ASCII-Table suitable for printing to a console.
-	 * 
-	 * @return a <code>String</code> holding  formatted table.
-	 */
-	public String createOutputTable() {
-
-		String output;
-		int livingWoodspace=15;
-		int deadWoodspace=23;
-		int harvestedWoodspace=16;
-		int processedWoddspace=18;
-		int boundCO2space=16;
-
-		output = "-----------------------------------------------------------------------------------------------------" ;
-		output += "| Jahr | lebendes Holz | verbleibendes Totholz | geerntets Holz | verwertetes Holz | gebundenes CO2 |";
-		output += "|  1   | 2,90908       | 0,89789               | 15,897234      | 18,7238409       | 2              |";
-		output += "-----------------------------------------------------------------------------------------------------" ;
-		return null;
+	public SimulationResult(int yearsSimulated) {
+		this.dataPerYears = new SimulationRecord[yearsSimulated];
 	}
 
-	public double getTotalLivingWood() {
-		return totalLivingWood;
+	public void addSimulationRecordForYear(SimulationRecord rec, int year) {
+
 	}
 
-	public void setTotalLivingWood(double totalLivingWood) {
-		this.totalLivingWood = totalLivingWood;
-	}
+	public class SimulationRecord {
 
-	public double getTotalDeadWood() {
-		return totalDeadWood;
-	}
+		/**
+		 * Formats the properties of this <code>SimulationResult</code> as an
+		 * ASCII-Table suitable for printing to a console.
+		 * 
+		 * @return a <code>String</code> holding formatted table.
+		 */
+		public String createOutputTable() {
 
-	public void setTotalDeadWood(double totalDeadWood) {
-		this.totalDeadWood = totalDeadWood;
-	}
+			String output;
+			int livingWoodspace = 15;
+			int deadWoodspace = 23;
+			int harvestedWoodspace = 16;
+			int processedWoddspace = 18;
+			int boundCO2space = 16;
 
-	public double getTotalHarvestedWood() {
-		return totalHarvestedWood;
-	}
+			output = "-----------------------------------------------------------------------------------------------------";
+			output += "| Jahr | lebendes Holz | verbleibendes Totholz | geerntets Holz | verwertetes Holz | gebundenes CO2 |";
+			output += "|  1   | 2,90908       | 0,89789               | 15,897234      | 18,7238409       | 2              |";
+			output += "-----------------------------------------------------------------------------------------------------";
+			return null;
+		}
 
-	public void setTotalHarvestedWood(double totalHarvestedWood) {
-		this.totalHarvestedWood = totalHarvestedWood;
-	}
+		/**
+		 * Total amount of living wood in the wood.
+		 */
+		private double totalLivingWood;
 
-	public double getTotalProcessedWood() {
-		return totalProcessedWood;
-	}
+		/**
+		 * Total amount of deadwood.
+		 */
+		private double totalDeadWood;
 
-	public void setTotalProcessedWood(double totalProcessedWood) {
-		this.totalProcessedWood = totalProcessedWood;
-	}
+		/**
+		 * The total amount of already harvested (but not yet processed) wood.
+		 */
+		private double totalHarvestedWood;
 
-	public double getTotalBoundCO2() {
-		return totalBoundCO2;
-	}
+		/**
+		 * The total amount of already processed wood.
+		 */
+		private double totalProcessedWood;
 
-	public void setTotalBoundCO2(double totalBoundCO2) {
-		this.totalBoundCO2 = totalBoundCO2;
+		/**
+		 * Total amount of carbon dioxide bound by the wood at the current point
+		 * in time.
+		 * 
+		 * NOTE: Could be calculated in the <code>Result</code>, for ease of
+		 * understanding not done here in order to cantralize all calculations.
+		 */
+		private double totalBoundCO2;
+
+		public double getTotalLivingWood() {
+			return totalLivingWood;
+		}
+
+		public void setTotalLivingWood(double totalLivingWood) {
+			this.totalLivingWood = totalLivingWood;
+		}
+
+		public double getTotalDeadWood() {
+			return totalDeadWood;
+		}
+
+		public void setTotalDeadWood(double totalDeadWood) {
+			this.totalDeadWood = totalDeadWood;
+		}
+
+		public double getTotalHarvestedWood() {
+			return totalHarvestedWood;
+		}
+
+		public void setTotalHarvestedWood(double totalHarvestedWood) {
+			this.totalHarvestedWood = totalHarvestedWood;
+		}
+
+		public double getTotalProcessedWood() {
+			return totalProcessedWood;
+		}
+
+		public void setTotalProcessedWood(double totalProcessedWood) {
+			this.totalProcessedWood = totalProcessedWood;
+		}
+
+		public double getTotalBoundCO2() {
+			return totalBoundCO2;
+		}
+
+		public void setTotalBoundCO2(double totalBoundCO2) {
+			this.totalBoundCO2 = totalBoundCO2;
+		}
+
 	}
 
 }
