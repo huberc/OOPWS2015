78a7b23dcf75aabdbb5b571466a8147c574c4ddc
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-10-20 04:22:45 +0200

    bugfixes in Siulator and SimulationResult moved all classes to default package

---
+++ oop-ue/src/main/java/SimulationResult.java
@@ -0,0 +1,213 @@
+
+
+
+/**
+ * This class describes the result of a simulation. The state of the environment
+ * is stored in a snapshot for each simulated year, i.e. the result has one
+ * <code>SimulatedRecord</code> for each year, including year zero, i.e. the
+ * initial state
+ * 
+ * @author Michael Langowski, e1426581student.tuwien.ac.at
+ *
+ */
+public class SimulationResult {
+
+	/**
+	 * internal storage for yearly snapshots.
+	 */
+	private SimulationRecord[] dataPerYears;
+
+	private int yearsSimulated;
+
+	/**
+	 * Creates a new <code>SimulationResult</code> with a pre-initialized
+	 * storage for <code>yearsSimulated + 1</code> years where the general
+	 * contract is that the "0-th" record holds the initial state.
+	 * 
+	 * @param yearsSimulated
+	 *            the number of years simulated
+	 */
+	public SimulationResult(int yearsSimulated) {
+		this.yearsSimulated = yearsSimulated;
+		this.dataPerYears = new SimulationRecord[yearsSimulated + 1];
+	}
+
+	/**
+	 * Adds a new simulation record (i.e. environment snapshot after the
+	 * <code>year</code>-th year) to this <code>SimulationResult</code>
+	 * 
+	 * @param rec
+	 *            the <code>SimulationRecord</code> to add
+	 * @param year
+	 *            the number of years elapsed in the simulation (where 0 would
+	 *            mean the initial state)
+	 */
+	public void addSimulationRecordForYear(SimulationRecord rec, int year) {
+		this.dataPerYears[year] = rec;
+	}
+
+	/**
+	 * Retrieves the <code>SimulationRecord</code> representing the environment
+	 * state after <code>year</code> years.
+	 * 
+	 * @param year
+	 *            the year in the simulation scenario that should be retrieved
+	 * @return the <code>SimulationRecord</code> representing the environment
+	 *         state after <code>year</code>
+	 */
+	public SimulationRecord getRecordForYear(int year) {
+		return this.dataPerYears[year];
+	}
+
+	public int getYearsSimulated() {
+		return yearsSimulated;
+	}
+
+	/**
+	 * A <code>SimulationRecord</code> represents the state of the simulated
+	 * environment at a specific point in time.
+	 * 
+	 * @author Michael Langowski, e142658@student.tuwien.ac.at
+	 *
+	 */
+	public class SimulationRecord {
+
+		/**
+		 * Total amount of living wood in the wood.
+		 */
+		private double totalLivingWood;
+
+		/**
+		 * Total amount of deadwood.
+		 */
+		private double totalDeadWood;
+
+		/**
+		 * The total amount of already harvested (but not yet processed) wood.
+		 */
+		private double totalHarvestedWood;
+
+		/**
+		 * The total amount of already processed wood.
+		 */
+		private double totalProcessedWood;
+
+		/**
+		 * Total amount of carbon dioxide bound by the wood at the current point
+		 * in time.
+		 * 
+		 * NOTE: Could be calculated in the <code>Result</code>, for ease of
+		 * understanding not done here in order to cantralize all calculations.
+		 */
+		private double totalBoundCO2;
+
+		public double getTotalLivingWood() {
+			return totalLivingWood;
+		}
+
+		public void setTotalLivingWood(double totalLivingWood) {
+			this.totalLivingWood = totalLivingWood;
+		}
+
+		public double getTotalDeadWood() {
+			return totalDeadWood;
+		}
+
+		public void setTotalDeadWood(double totalDeadWood) {
+			this.totalDeadWood = totalDeadWood;
+		}
+
+		public double getTotalHarvestedWood() {
+			return totalHarvestedWood;
+		}
+
+		public void setTotalHarvestedWood(double totalHarvestedWood) {
+			this.totalHarvestedWood = totalHarvestedWood;
+		}
+
+		public double getTotalProcessedWood() {
+			return totalProcessedWood;
+		}
+
+		public void setTotalProcessedWood(double totalProcessedWood) {
+			this.totalProcessedWood = totalProcessedWood;
+		}
+
+		public double getTotalBoundCO2() {
+			return totalBoundCO2;
+		}
+
+		public void setTotalBoundCO2(double totalBoundCO2) {
+			this.totalBoundCO2 = totalBoundCO2;
+		}
+
+        public String toString() {
+            int livingWoodspace = 15;
+            int deadWoodspace = 23;
+            int harvestedWoodspace = 20;
+            int processedWoddspace = 20;
+            int boundCO2space = 20;
+
+            String line = "";
+            
+            line += "|" + totalLivingWood;
+            if(String.valueOf(totalLivingWood).length() < livingWoodspace) {
+                for(int i = String.valueOf(totalLivingWood).length(); i < livingWoodspace; i++) {
+                    line += " ";
+                }
+            }
+
+            line += "|" + totalDeadWood;
+            if(String.valueOf(totalDeadWood).length() < deadWoodspace) {
+                for(int i = String.valueOf(totalDeadWood).length(); i < deadWoodspace; i++) {
+                    line += " ";
+                }
+            }
+
+            line += "|" + totalHarvestedWood;
+            if(String.valueOf(totalHarvestedWood).length() < harvestedWoodspace) {
+                for(int i = String.valueOf(totalHarvestedWood).length(); i < harvestedWoodspace; i++) {
+                    line += " ";
+                }
+            }
+
+            line += "|" + totalProcessedWood;
+            if(String.valueOf(totalProcessedWood).length() < processedWoddspace) {
+                for(int i = String.valueOf(totalProcessedWood).length(); i < processedWoddspace; i++) {
+                    line += " ";
+                }
+            }
+
+            line += "|" + totalBoundCO2;
+            if(String.valueOf(totalBoundCO2).length() < boundCO2space) {
+                for(int i = String.valueOf(totalBoundCO2).length(); i < boundCO2space; i++) {
+                    line += " ";
+                }
+            }
+
+            return line + "| \n";
+		}
+	}
+
+	public String toString() {
+		int yearSpace = 6;
+		
+        String output;
+
+        output = "\n" + "--------------------------------------------------------------------------------------------------------------- \n";
+        output += "| Jahr | lebendes Holz | verbleibendes Totholz |   geerntets Holz   |  verwertetes Holz  |   gebundenes CO2   | \n";
+        String line;
+        for(int i = 0; i < this.dataPerYears.length; i++){
+            line = "|" + i;
+            for(int j = String.valueOf(i).length(); j < yearSpace; j++) {
+                line += " ";
+            }
+            line += this.dataPerYears[i].toString();
+            output += line;
+        }
+        output += "---------------------------------------------------------------------------------------------------------------";
+
+        return output;
+	}
+
+}
---
+++ oop-ue/src/main/java/Simulator.java
@@ -0,0 +1,71 @@
+
+
+public class Simulator {
+
+	/**
+	 * Executes a simulation run based on a given <code>SimulationRequest</code>
+	 * for a given number of years.
+	 * 
+	 * @param req
+	 *            the request holding all simulation input params
+	 * @param years
+	 *            the number of years to simulate
+	 * @return a <code>SimulationResult</code> describing the state of the wood
+	 *         after <code>years</code> years
+	 */
+	public SimulationResult simulate(SimulationRequest req, int years) {
+
+		double totalLivingWoodpast = 0;
+		double totalDeadWoodpast = 0;
+		double totalHarvestedWoodpast = 0;
+		double totalProcessedWoodpast = 0;
+
+		SimulationResult retVal = new SimulationResult(years);
+		SimulationResult.SimulationRecord rec;
+
+		for(int i=0;i<=years;i++){
+
+			 rec = retVal.new SimulationRecord();
+
+			if(i==0){
+
+				rec.setTotalLivingWood(req.getStartLivingWood());
+				totalLivingWoodpast = req.getStartLivingWood();
+
+				rec.setTotalDeadWood(req.getStartDeadWood());
+				totalDeadWoodpast = req.getStartDeadWood();
+
+				rec.setTotalHarvestedWood(0);
+
+				rec.setTotalProcessedWood(0);
+				totalProcessedWoodpast = 0;
+
+				rec.setTotalBoundCO2(req.getStartLivingWood() + req.getStartDeadWood());
+			}
+			else{
+				// BUGFIX - living wood must also be reduced by dead wood (-> dead wood doesn't just grow)
+				//rec.setTotalLivingWood(totalLivingWoodpast + req.getAvgWoodGrowth() - req.getAvgHarvestYearly());
+				rec.setTotalLivingWood(totalLivingWoodpast + req.getAvgWoodGrowth() - req.getAvgHarvestYearly() - req.getAvgDeadWoodYearly());
+				totalLivingWoodpast = totalLivingWoodpast + req.getAvgWoodGrowth() - req.getAvgHarvestYearly() - req.getAvgDeadWoodYearly();
+
+				// BUGFIX - deadwood should only start rotting after 1 year
+				rec.setTotalDeadWood((totalDeadWoodpast * (1-req.getAvgDecompWoodYearly()) + req.getAvgDeadWoodYearly()));
+				totalDeadWoodpast = (totalDeadWoodpast  * (1-req.getAvgDecompWoodYearly()) + req.getAvgDeadWoodYearly());
+
+				// BUGFIX - need to consider previously harvested wood
+				//rec.setTotalHarvestedWood(req.getAvgHarvestYearly() * (1-req.getAvgProcessedWoodYearly()));
+				rec.setTotalHarvestedWood(totalHarvestedWoodpast + req.getAvgHarvestYearly() * (1-req.getAvgProcessedWoodYearly()));
+				totalHarvestedWoodpast = totalHarvestedWoodpast + req.getAvgHarvestYearly() * (1-req.getAvgProcessedWoodYearly());
+
+				rec.setTotalProcessedWood(totalProcessedWoodpast + (req.getAvgHarvestYearly()*req.getAvgProcessedWoodYearly()));
+				totalProcessedWoodpast = totalProcessedWoodpast + (req.getAvgHarvestYearly()*req.getAvgProcessedWoodYearly());
+
+				rec.setTotalBoundCO2(totalLivingWoodpast + totalDeadWoodpast + totalHarvestedWoodpast);
+			}
+
+			retVal.addSimulationRecordForYear(rec,i);
+		}
+
+		return retVal;
+	}
+}
