5e18d793e091bbb534d8fa52d0217504a8be93e1
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-10-25 02:27:39 +0100

    realistic weather model done - TODO more models

--- oop-ue/src/main/java/RealWorldWeatherModel.java
+++ oop-ue/src/main/java/RealWorldWeatherModel.java
@@ -2,11 +2,27 @@ import java.util.Random;
 import java.util.function.Function;
 
 /**
- * A "real-world" climate model, roughly modeling actual climatic tendencies.
- * Historical data URL:
- * <link>https://www.innsbruck.gv.at/page.cfm?vpath=verwaltung
- * /statistiken--zahlen/klima</link>
+ * A "real-world" climate model, trying to roughly model actual climatic tendencies.
  * 
+ * Calculation of temperature and rainfall values (which are to be understood as averages
+ * for a simulation year) is done based on regression functions based on the recorded 
+ * yearly average temperature and rainfall values in Innsbruck between 1906 and 2012.
+ * All calculations performed by a <code>RealWorldWeatherModel</code> assume 2015
+ * to be the year 0 of the simulation.
+ * 
+ * Historical were values obtained from:
+ * <link>https://www.innsbruck.gv.at/page.cfm?vpath=verwaltung/statistiken--zahlen/klima</link>
+ * 
+ * Depending on the used <code>ModelingApproach</code>, the used regression function
+ * is either:
+ * 	Linear -> temp(year) = a * year + b
+ *  Exponential -> temp(year) = a * e^(b * temp)
+ *  Logarithmic -> temp(year) = a * ln(year) + b
+ *  
+ * Optionally - depending on the <code>addSpreading</code> property, a random component
+ * is added to the results returned by above described functions. 
+ * The random component is "normally" (i.e. gaussian) distributed.
+ *  
  * @author michael
  *
  */
@@ -46,12 +62,6 @@ public class RealWorldWeatherModel implements IWeatherModel {
 		}
 
 	}
-	
-	private ModelingApproach approach;
-	
-	private boolean addSpreading;
-	
-	private Random randomNumberGen = new Random((long) (Math.random() * System.currentTimeMillis()));
 
 	/**
 	 * The x-offset to use for input values of calculation functions. Assuming
@@ -65,7 +75,7 @@ public class RealWorldWeatherModel implements IWeatherModel {
 	private static final double RAIN_STDDEV = 127.3405555;
 
 	private static final Function<Integer, Double> TEMP_LINEAR = (year) -> {
-		return 0.0176433708 * (year) + 7.7536863967;
+		return 0.0176433708 * year + 7.7536863967;
 	};
 
 	private static final Function<Integer, Double> TEMP_EXPONENTIAL = (year) -> {
@@ -87,6 +97,17 @@ public class RealWorldWeatherModel implements IWeatherModel {
 	private static final Function<Integer, Double> RAIN_LOGARITHMIC = (year) -> {
 		return -12.371339315 * Math.log(year) + 949.9769025133;
 	};
+	
+	private ModelingApproach approach;
+	
+	private boolean addSpreading;
+	
+	private Random randomNumberGen = new Random((long) (Math.random() * System.currentTimeMillis()));
+	
+	public RealWorldWeatherModel(ModelingApproach approach, boolean doSpreading){
+		this.approach = approach;
+		this.addSpreading = doSpreading;
+	}
 	
 	@Override
 	public WeatherConditions calcWeatherForYear(int simulationYear) {
