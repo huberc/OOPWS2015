6f8e672fe206e85f291da95352faa84fbef494fb
huberc <huberc6@gmail.com>
2015-11-10 12:25:29 +0100

    ettiketierbar auf false gesetzt bei den Bauholz arten

--- oop-ue/src/main/java/BauRundholz.java
+++ oop-ue/src/main/java/BauRundholz.java
@@ -5,10 +5,13 @@ public class BauRundholz extends Saegerundholz implements Bauholz {
     public BauRundholz(int laenge, int staerke) {
         super(laenge, staerke);
 
+        this.setEtiketierbar(false);
+
         if (staerke < 20)
             throw new IllegalArgumentException("Ein Holz mit der Staerke " + staerke + " cm kann kein Bau-Rundholz sein");
         if (laenge < 50)
             throw new IllegalArgumentException("Ein Holz der laenge " + laenge + " cm ist zu kurz um als Bau-Rundholz verwendet werden zu koennen");
+
     }
 
 
--- oop-ue/src/main/java/Kantholz.java
+++ oop-ue/src/main/java/Kantholz.java
@@ -5,6 +5,8 @@ public class Kantholz extends Vollkantschnittholz implements Bauholz {
     public Kantholz(int laenge, int dicke, int breite) {
         super(laenge, dicke, breite);
 
+        this.setEtiketierbar(false);
+
         if (dicke < 4)
             throw new IllegalArgumentException("Ein Kantholz muss mindestens eine dicke von 4 cm aufweisen");
         if ((breite > (dicke * 3)) || breite < 1)
