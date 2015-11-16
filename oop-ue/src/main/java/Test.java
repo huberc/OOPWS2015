f9f302508db849cf8abead0da1a81c89a7c89b8a
huberc <huberc6@gmail.com>
2015-11-16 14:04:18 +0100

    test angepasst

--- oop-ue/src/main/java/Test.java
+++ oop-ue/src/main/java/Test.java
@@ -1,43 +1,54 @@
+import java.util.Iterator;
+
 /**
  * @author Christoph Huber e1427322@student.tuwien.ac.at
  */
 public class Test {
+    private BasicSet<Integer> integerBasicSet = new BasicSet<>();
+
+    private SortedSet<MyInteger> integerSortedSet = new SortedSet<>();
+
+    private SortedSet<Baurundholz> baurundholzSortedSet = new SortedSet<>();
+
+    private SortedSet<Baukantholz> baukantholzSortedSet = new SortedSet<>();
 
     public static void main(String[] args) {
 
     }
 
-    public static boolean Test1(){
+    public void Test1(){
 
-        BasicSet<Integer> integerBasicSet = new BasicSet<>();
-
+        // create Sets
         integerBasicSet.add(1);
         integerBasicSet.add(5);
         integerBasicSet.add(2);
         integerBasicSet.add(8);
-
-        SortedSet<MyInteger> integerSortedSet = new SortedSet<>();
 
         integerSortedSet.add(new MyInteger(1));
         integerSortedSet.add(new MyInteger(9));
         integerSortedSet.add(new MyInteger(4));
         integerSortedSet.add(new MyInteger(3));
 
-        SortedSet<Baurundholz> baurundholzSortedSet = new SortedSet<>();
-
         baurundholzSortedSet.add(new Baurundholz(10,20));
         baurundholzSortedSet.add(new Baurundholz(20,35));
         baurundholzSortedSet.add(new Baurundholz(15,10));
         baurundholzSortedSet.add(new Baurundholz(45,15));
 
-        SortedSet<Baukantholz> baukantholzSortedSet = new SortedSet<>();
 
         baukantholzSortedSet.add(new Baukantholz(10,10,10));
         baukantholzSortedSet.add(new Baukantholz(15,20,50));
         baukantholzSortedSet.add(new Baukantholz(5,5,15));
         baukantholzSortedSet.add(new Baukantholz(20,55,45));
 
-        return false;
+
+        System.out.println(integerBasicSet.toString());
+        System.out.println(integerSortedSet.toString());
+        System.out.println(baurundholzSortedSet.toString());
+        System.out.println(baukantholzSortedSet.toString());
+
+        // remove and add new on
+        integerBasicSet.iterator().next().re,p;
+
     }
 
     public static boolean Test2(){
