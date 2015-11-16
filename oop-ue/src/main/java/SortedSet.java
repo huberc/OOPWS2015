3cac46f94eae5dbcbf6c041119245db10fd63d82
Michael Langowski <michael.langowski@sonydadc.com>
2015-11-16 15:28:30 +0100

    fixed the shit out of it

--- oop-ue/src/main/java/SortedSet.java
+++ oop-ue/src/main/java/SortedSet.java
@@ -8,12 +8,24 @@ public class SortedSet<T extends Smaller<? super T>> extends BasicSet<T> {
 
 	@Override
 	public void add(T value) {
+		ListElem<T> elem = new ListElem<>(value);
+		if(this.head == null){
+			this.head = elem;
+		}
+		if(this.head.getNext() == null){
+			if(value.smaller(this.head.getValue())){
+				elem.setNext(this.head);
+				this.head = elem;
+			}else{
+				this.head.setNext(elem);
+			}
+			return;
+		}
 		ListElem<T> current = this.head;
 		while(current != null){
 			if(value.smaller(current.getValue())){
 				if(current.getNext() == null || current.getNext().getValue().smaller(value)){
 					// insert here
-					ListElem<T> elem = new ListElem<>(value);
 					elem.setNext(current.getNext());
 					current.setNext(elem);
 					break;
