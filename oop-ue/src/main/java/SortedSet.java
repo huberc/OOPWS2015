e9fc7f1fff1b95e11733b9f71de0ee33b309c27b
Michael Langowski <michael.langowski@sonydadc.com>
2015-11-16 15:47:27 +0100

    should work - if not dont blame me

--- oop-ue/src/main/java/SortedSet.java
+++ oop-ue/src/main/java/SortedSet.java
@@ -9,30 +9,50 @@ public class SortedSet<T extends Smaller<? super T>> extends BasicSet<T> {
 	@Override
 	public void add(T value) {
 		ListElem<T> elem = new ListElem<>(value);
-		if(this.head == null){
+		if (this.head == null) {
 			this.head = elem;
-		}
-		if(this.head.getNext() == null){
-			if(value.smaller(this.head.getValue())){
-				elem.setNext(this.head);
-				this.head = elem;
-			}else{
-				this.head.setNext(elem);
-			}
 			return;
 		}
+		ListElem<T> previous = null;
 		ListElem<T> current = this.head;
-		while(current != null){
-			if(value.smaller(current.getValue())){
-				if(current.getNext() == null || current.getNext().getValue().smaller(value)){
-					// insert here
-					elem.setNext(current.getNext());
-					current.setNext(elem);
-					break;
+		do {
+			if (!(current.getValue().smaller(value))) {
+				elem.setNext(current);
+				if (previous != null) {
+					previous.setNext(elem);
+				} else {
+					this.head.setNext(elem);
 				}
-				current = current.getNext();
+				return;
 			}
-		}
+			previous = current;
+			current = current.getNext();
+		} while (current != null);
+		// element must be added at last position
+		previous.setNext(elem);
+
+		// if(this.head.getNext() == null){
+		// if(value.smaller(this.head.getValue())){
+		// elem.setNext(this.head);
+		// this.head = elem;
+		// }else{
+		// this.head.setNext(elem);
+		// }
+		// return;
+		// }
+		// ListElem<T> current = this.head;
+		// while(current != null){
+		// if(value.smaller(current.getValue())){
+		// if(current.getNext() == null ||
+		// current.getNext().getValue().smaller(value)){
+		// // insert here
+		// elem.setNext(current.getNext());
+		// current.setNext(elem);
+		// break;
+		// }
+		// current = current.getNext();
+		// }
+		// }
 	}
 
 }
