6d390f075af3e487ff5170dde2450fcd3c93bc94
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-11-19 21:38:24 +0100

    added ernter

---
+++ oop-ue/src/main/java/Radernter.java
@@ -0,0 +1,11 @@
+
+
+public class Radernter extends Holzvollernter{
+
+    @Override
+    public void addDistanceMoved(Number distanceMoved) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ oop-ue/src/main/java/Schreiter.java
@@ -0,0 +1,11 @@
+
+
+public class Schreiter extends Holzvollernter{
+
+    @Override
+    public void addDistanceMoved(Number distanceMoved) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
