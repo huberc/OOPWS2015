af0ebde7f373ac7d14e69e1e60be362c0ad0932e
huberc <huberc6@gmail.com>
2015-11-19 21:03:53 +0100

    touched...

--- oop-ue/src/main/java/ObjectList.java
+++ oop-ue/src/main/java/ObjectList.java
@@ -6,58 +6,76 @@ public class ObjectList {
     private ObjectListNode head = null;
 
     /**
+     * VB: elem darf nicht null sein
+     * NB: elem ist Bestandteil der Liste
      *
      * @param elem das einzufuegende Element in die "Liste"
-     *
      */
-    void insert(Object elem){
-        if(elem == null){
-            throw new NullPointerException("Das einzufuegende Element ist null und nicht erlaubt");
-        }else{
-            if(head == null)
-                this.head = new ObjectListNode(elem);
-            else if (!(contains(elem))) {
-                ObjectListNode tmp = head;
-                this.head = new ObjectListNode(elem);
-                this.head.next = tmp;
-            }
+    void insert(Object elem) {
+
+        if (head == null)
+            this.head = new ObjectListNode(elem);
+        else if (!(contains(elem))) {
+            ObjectListNode tmp = head;
+            this.head = new ObjectListNode(elem);
+            this.head.next = tmp;
         }
+
     }
 
-    public ObjectListNode getHead(){
+
+    /**
+     * VB: name darf nicht null sein
+     * NB: null oder das entsprechende Objekt wird dem Client zurueckgegeben.
+     *
+     * @param name String in der Liste
+     * @return Object oder null fals der Name nicht vorhanden ist
+     */
+    Object removeByName(String name){
+
+        ObjectListNode cur = head;
+
+        while (cur.next != null){
+            if(cur.value.equals(name)){
+                return cur.value;
+            }
+            else{
+                cur = cur.next;
+            }
+        }
+        return null;
+    }
+
+
+    public ObjectListNode getHead() {
         return this.head;
     }
 
 
-    class ObjectListNode{
-        private ObjectListNode next=null;
+    class ObjectListNode {
+        private ObjectListNode next = null;
         private Object value;
 
-        public ObjectListNode(Object value){
+        public ObjectListNode(Object value) {
             this.value = value;
-        }
-
-        public ObjectListNode getNext(){
-            return this.next;
         }
     }
 
     /**
-     *  Die Methode ueberprueft, ob das uebergebene Element bereits in der "Liste" vorhanden ist.
+     * Die Methode ueberprueft, ob das uebergebene Element bereits in der "Liste" vorhanden ist.
      *
-     * @param elem
-     *          vom Typ Object
+     * VB: elem darf nicht null sein
      *
+     * @param elem vom Typ Object
      * @return true, falls das uebergebene Element schon vorhanden ist, ansonsten false
      */
-    boolean contains(Object elem){
+    boolean contains(Object elem) {
         ObjectListNode cur = head;
-        while((cur.getNext() != null)){
-            if(cur.equals(elem)){
+        while ((cur.next != null)) {
+            if (cur.equals(elem)) {
                 return true;
-            }
-            else{
-                cur = cur.getNext();
+            } else {
+                cur = cur.next;
             }
         }
         return false;
