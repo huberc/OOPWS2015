40f50310afdbac52f80851320f91d3f269ea7143
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-11-19 21:34:20 +0100

    made the holzvollernter listable

--- oop-ue/src/main/java/Holzvollernter.java
+++ oop-ue/src/main/java/Holzvollernter.java
@@ -1,67 +1,69 @@
 /**
  * @author Michael Langowski
  */
-public abstract class Holzvollernter implements Listable{
+public abstract class Holzvollernter implements Listable {
 
-	private static Integer idSequence = 1;
+    private static Integer idSequence = 1;
 
-	/**
-	 * Ensure uniqueness of IDs by making it only accessible through this method
-	 * 
-	 * @return the current value of the id sequence, i.e. the next id to assign
-	 *         to a new "Vollernter" (1-based). After the current id value was
-	 *         fetched, the sequence is incremented. This method is thread-safe
-	 */
-	protected static final int fetchId() {
-		int retVal;
-		synchronized (Holzvollernter.idSequence) {
-			retVal = Holzvollernter.idSequence++;
-		}
-		return retVal;
-	}
+    /**
+     * Ensure uniqueness of IDs by making it only accessible through this method
+     * 
+     * @return the current value of the id sequence, i.e. the next id to assign to a new "Vollernter"
+     *         (1-based). After the current id value was fetched, the sequence is incremented. This method is
+     *         thread-safe
+     */
+    protected static final int fetchId() {
+        int retVal;
+        synchronized (Holzvollernter.idSequence) {
+            retVal = Holzvollernter.idSequence++;
+        }
+        return retVal;
+    }
 
-	private int id;
-	private AbstractArbeitskopf arbeitskopf;
-	private Number distanceMoved;
-	private int betriebsstunden;
+    private int                 id;
+    private AbstractArbeitskopf arbeitskopf;
+    private Number              distanceMoved;
+    private int                 betriebsstunden;
 
-	public AbstractArbeitskopf getArbeitskopf() {
-		return arbeitskopf;
-	}
+    public AbstractArbeitskopf getArbeitskopf() {
+        return arbeitskopf;
+    }
 
-	public void setArbeitskopf(AbstractArbeitskopf arbeitskopf) {
-		this.arbeitskopf = arbeitskopf;
-	}
+    public void setArbeitskopf(AbstractArbeitskopf arbeitskopf) {
+        this.arbeitskopf = arbeitskopf;
+    }
 
-	public Number getDistanceMoved() {
-		return distanceMoved;
-	}
+    public Number getDistanceMoved() {
+        return distanceMoved;
+    }
 
-	/**
-	 * Implementation-defined method to add a distance value to the total
-	 * distance this <code>Holzvollernter</code> moved. Depending on the manner
-	 * of moving of this vollernter, implementations my treat the passed-in
-	 * number differently.
-	 * 
-	 * @param distanceMoved
-	 *            a <code>Number</code> describing the distance this vollernter
-	 *            moved during its time in service. Depending on the actual
-	 *            distance unit used by the implementation loss of precision in
-	 *            input number is possible. The value passed here is added to
-	 *            the current total distance the vollernter moved
-	 */
-	public abstract void addDistanceMoved(Number distanceMoved);
+    /**
+     * Implementation-defined method to add a distance value to the total distance this
+     * <code>Holzvollernter</code> moved. Depending on the manner of moving of this vollernter,
+     * implementations my treat the passed-in number differently.
+     * 
+     * @param distanceMoved
+     *            a <code>Number</code> describing the distance this vollernter moved during its time in
+     *            service. Depending on the actual distance unit used by the implementation loss of precision
+     *            in input number is possible. The value passed here is added to the current total distance
+     *            the vollernter moved
+     */
+    public abstract void addDistanceMoved(Number distanceMoved);
 
-	public int getId() {
-		return id;
-	}
-   
+    public int getId() {
+        return id;
+    }
+
     public int getBetriebsstunden() {
         return betriebsstunden;
     }
-  
+
     public void setBetriebsstunden(int betriebsstunden) {
         this.betriebsstunden = betriebsstunden;
     }
-    
+
+    public String getName() {
+        return Integer.toString(this.id);
+    }
+
 }
