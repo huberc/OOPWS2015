0e8c223b508df0be3c954dd0485f4c1068b562f6
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-11-23 01:29:27 +0100

    intermediate commit

--- oop-ue/src/main/java/Schreiter.java
+++ oop-ue/src/main/java/Schreiter.java
@@ -1,9 +1,28 @@
+/**
+ * Implementierung eines <code>Holzvollernter</code>, der sich auf Schreitbeinen bewegt. Die zurueckgelegte
+ * Distanz ist dabei eine gaze Zahl und stellt die Anzahl der bisherigen Schritte des Ernters dar.
+ * 
+ * @author Michael Langowski, e1426581@student.tuwien.ac.at
+ *
+ */
+public class Schreiter extends Holzvollernter {
 
-
-public class Schreiter extends Holzvollernter{
-
+    /**
+     * Addiert einen gegebenen Wert zur aktuell zurueckgelegten Distanz des Ernters. Die Distanz wird dabei
+     * nicht ueberschrieben sondern erhoeht
+     * 
+     * @param distanceMoved
+     *            Ganze Zahl, der die seit dem letzten Aufruf von <code>addDistanceMoved</code> zurueckgelegte
+     *            Distanz repraesentiert. Muss > 0 sein.
+     * 
+     * @throws IllegalArgumentException
+     *             falls <code>distanceMoved</code> nicht der Spezifikation entspricht, d.h. <= 0 ist
+     */
     @Override
-    public void addDistanceMoved(Number distanceMoved) {
+    public void addDistanceMoved(Number distanceMoved) throws IllegalArgumentException {
+        if ((int) distanceMoved <= 0) {
+            throw new IllegalArgumentException("Zurueckgelegte Distanz muss groesser 0 sein");
+        }
         int dist = (int) this.distanceMoved;
         dist += (int) distanceMoved;
         this.distanceMoved = dist;
