41cb8bf40e5d4e6b2cdcaf6bf505fb1dc6858527
huberc <huberc6@gmail.com>
2015-11-23 13:10:26 +0100

    klassenvvariablen gmocht

--- oop-ue/src/main/java/Test.java
+++ oop-ue/src/main/java/Test.java
@@ -5,6 +5,34 @@
  */
 
 public class Test {
+    Gemeinde gemeinde1 = new Gemeinde("gemeinde1");
+    Gemeinde gemeinde2 = new Gemeinde("gemeinde2");
+    Gemeinde gemeinde3 = new Gemeinde("gemeinde3");
+
+    Forstbetrieb forstbetrieb11 = new Forstbetrieb("forstbetrieb11");
+    Forstbetrieb forstbetrieb12 = new Forstbetrieb("forstbetrieb12");
+    Forstbetrieb forstbetrieb13 = new Forstbetrieb("forstbetrieb13");
+
+    Forstbetrieb forstbetrieb21 = new Forstbetrieb("forstbetrieb21");
+    Forstbetrieb forstbetrieb22 = new Forstbetrieb("forstbetrieb22");
+    Forstbetrieb forstbetrieb23 = new Forstbetrieb("forstbetrieb23");
+
+    Forstbetrieb forstbetrieb31 = new Forstbetrieb("forstbetrieb31");
+    Forstbetrieb forstbetrieb32 = new Forstbetrieb("forstbetrieb32");
+    Forstbetrieb forstbetrieb33 = new Forstbetrieb("forstbetrieb33");
+
+    Radernter ernter1 = new Radernter();
+    Schreiter ernter2 = new Schreiter();
+    Radernter ernter3 = new Radernter();
+    Schreiter ernter4 = new Schreiter();
+    Radernter ernter5 = new Radernter();
+    Schreiter ernter6 = new Schreiter();
+    Radernter ernter7 = new Radernter();
+    Schreiter ernter8 = new Schreiter();
+    Radernter ernter9 = new Radernter();
+    Schreiter ernter10 = new Schreiter();
+    Radernter ernter11 = new Radernter();
+
 
     public static void main(String[] args) {
         Test test = new Test();
@@ -13,10 +41,10 @@ public class Test {
 
     public boolean test() {
         //3 Gemeinden erstellen, je 3 Forstbetriebe hinzufuegen und einen wieder entfernen (ueber eindeutigen Namen)
-        Gemeinde gemeinde1 = new Gemeinde("gemeinde1");
-        gemeinde1.addBetrieb("forstbetrieb11");
-        gemeinde1.addBetrieb("forstbetrieb12");
-        gemeinde1.addBetrieb("forstbetrieb13");
+
+        gemeinde1.addBetrieb();
+        gemeinde1.addBetrieb();
+        gemeinde1.addBetrieb();
         gemeinde1.deleteBetrieb("forstbetrieb12");
 
         if ((!gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb11"))) || (gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb12"))) || (!gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb13")))) {
@@ -24,10 +52,10 @@ public class Test {
             return false;
         }
 
-        Gemeinde gemeinde2 = new Gemeinde("gemeinde2");
-        gemeinde2.addBetrieb("forstbetrieb21");
-        gemeinde2.addBetrieb("forstbetrieb22");
-        gemeinde2.addBetrieb("forstbetrieb23");
+
+        gemeinde2.addBetrieb();
+        gemeinde2.addBetrieb();
+        gemeinde2.addBetrieb();
         gemeinde2.deleteBetrieb("forstbetrieb22");
 
         if ((!gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb21"))) || (gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb22"))) || (!gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb23")))) {
@@ -35,9 +63,9 @@ public class Test {
             return false;
         }
 
-        Gemeinde gemeinde3 = new Gemeinde("gemeinde3");
-        gemeinde3.addBetrieb("forstbetrieb31");
-        gemeinde3.addBetrieb("forstbetrieb32");
+
+        gemeinde3.addBetrieb();
+        gemeinde3.addBetrieb();
         gemeinde3.addBetrieb("forstbetrieb33");
         gemeinde3.deleteBetrieb("forstbetrieb32");
 
@@ -47,23 +75,23 @@ public class Test {
         }
 
         //Holzvollernter hinzufuegen/ entfernen/ Informationen aendern (ueber Name und Nummer)
-        Radernter ernter1 = new Radernter();
+
         ernter1.setArbeitskopf(new Schneidearbeitskopf(20.5));
         gemeinde1.getBetriebByName("forstbetrieb11").addHolzvollernter(ernter1); // TODO anpassen
         gemeinde1.getBetriebByName("forstbetrieb11").changeInformationOfHolzvollernter(1, 24, 10.0, new Schneidearbeitskopf(100.0));
-        Schreiter ernter2 = new Schreiter();
+
         gemeinde1.getBetriebByName("forstbetrieb13").addHolzvollernter(ernter2);
         gemeinde1.getBetriebByName("forstbetrieb13").changeInformationOfHolzvollernter(2, 32, 15,
                 new HackschnitzelArbeitskopf(10));
-        Radernter ernter3 = new Radernter();
+
         gemeinde1.getBetriebByName("forstbetrieb13").addHolzvollernter(ernter3);
         gemeinde1.getBetriebByName("forstbetrieb13").changeInformationOfHolzvollernter(3, 48, 13.0,
                 new Schneidearbeitskopf(75.0));
-        Schreiter ernter4 = new Schreiter();
+
         gemeinde1.getBetriebByName("forstbetrieb13").addHolzvollernter(ernter4);
         gemeinde1.getBetriebByName("forstbetrieb13").changeInformationOfHolzvollernter(4, 60, 20,
                 new HackschnitzelArbeitskopf(10));
-        Radernter ernter5 = new Radernter();
+
         gemeinde1.getBetriebByName("forstbetrieb13").addHolzvollernter(ernter5);
         gemeinde1.getBetriebByName("forstbetrieb13").changeInformationOfHolzvollernter(5, 72, 13.0,
                 new Schneidearbeitskopf(125.0));
@@ -81,15 +109,14 @@ public class Test {
             return false;
         }
 
-        Schreiter ernter6 = new Schreiter();
         gemeinde2.getBetriebByName("forstbetrieb21").addHolzvollernter(ernter6);
         gemeinde2.getBetriebByName("forstbetrieb21").changeInformationOfHolzvollernter(6, 24, 32,
                 new Schneidearbeitskopf(100.0));
-        Radernter ernter7 = new Radernter();
+
         gemeinde2.getBetriebByName("forstbetrieb23").addHolzvollernter(ernter7);
         gemeinde2.getBetriebByName("forstbetrieb23").changeInformationOfHolzvollernter(7, 32, 10.0,
                 new HackschnitzelArbeitskopf(10));
-        Schreiter ernter8 = new Schreiter();
+
         gemeinde2.getBetriebByName("forstbetrieb23").addHolzvollernter(ernter8);
         gemeinde2.getBetriebByName("forstbetrieb23").changeInformationOfHolzvollernter(8, 28, 13,
                 new HackschnitzelArbeitskopf(10));
@@ -101,15 +128,14 @@ public class Test {
             return false;
         }
 
-        Radernter ernter9 = new Radernter();
         gemeinde3.getBetriebByName("forstbetrieb31").addHolzvollernter(ernter9);
         gemeinde3.getBetriebByName("forstbetrieb31").changeInformationOfHolzvollernter(9, 24, 20.0,
                 new Schneidearbeitskopf(100.0));
-        Schreiter ernter10 = new Schreiter();
+
         gemeinde3.getBetriebByName("forstbetrieb33").addHolzvollernter(ernter10);
         gemeinde3.getBetriebByName("forstbetrieb33").changeInformationOfHolzvollernter(10, 32, 10,
                 new HackschnitzelArbeitskopf(10));
-        Radernter ernter11 = new Radernter();
+
         gemeinde3.getBetriebByName("forstbetrieb33").addHolzvollernter(ernter11);
         gemeinde3.getBetriebByName("forstbetrieb33").changeInformationOfHolzvollernter(11, 48, 13.0,
                 new HackschnitzelArbeitskopf(10));
@@ -135,6 +161,10 @@ public class Test {
         gemeinde1.getBetriebByName("forstbetrieb13").getAvgTreeThickness(); */
 
         return true;
+    }
+
+    public boolean testStatistiken(){
+
     }
 
 }
