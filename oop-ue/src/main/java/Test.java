6cd9621430340970588020c4354e13d95277b12a
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-11-23 13:33:09 +0100

    eat this

--- oop-ue/src/main/java/Test.java
+++ oop-ue/src/main/java/Test.java
@@ -1,13 +1,13 @@
 /**
- * Arbeitsaufteilung: Christoph Huber: Forstbetrieb, CustomList, ListableNumber,
- * Michael Langowksi: Holzvollernter, Abstractarbeitskopf
- * Ines Rieder: Gemeinde, Test
+ * Arbeitsaufteilung: Christoph Huber: Forstbetrieb, CustomList, ListableNumber, Michael Langowksi:
+ * Holzvollernter, Abstractarbeitskopf Ines Rieder: Gemeinde, Test
  */
 
 public class Test {
-    Gemeinde gemeinde1 = new Gemeinde("gemeinde1");
-    Gemeinde gemeinde2 = new Gemeinde("gemeinde2");
-    Gemeinde gemeinde3 = new Gemeinde("gemeinde3");
+
+    Gemeinde     gemeinde1      = new Gemeinde("gemeinde1");
+    Gemeinde     gemeinde2      = new Gemeinde("gemeinde2");
+    Gemeinde     gemeinde3      = new Gemeinde("gemeinde3");
 
     Forstbetrieb forstbetrieb11 = new Forstbetrieb("forstbetrieb11");
     Forstbetrieb forstbetrieb12 = new Forstbetrieb("forstbetrieb12");
@@ -20,47 +20,18 @@ public class Test {
     Forstbetrieb forstbetrieb31 = new Forstbetrieb("forstbetrieb31");
     Forstbetrieb forstbetrieb32 = new Forstbetrieb("forstbetrieb32");
     Forstbetrieb forstbetrieb33 = new Forstbetrieb("forstbetrieb33");
-    Gemeinde gemeinde1 = new Gemeinde("gemeinde1");
-    Gemeinde gemeinde2 = new Gemeinde("gemeinde2");
-    Gemeinde gemeinde3 = new Gemeinde("gemeinde3");
 
-    Forstbetrieb forstbetrieb11 = new Forstbetrieb("forstbetrieb11");
-    Forstbetrieb forstbetrieb12 = new Forstbetrieb("forstbetrieb12");
-    Forstbetrieb forstbetrieb13 = new Forstbetrieb("forstbetrieb13");
-
-    Radernter ernter1 = new Radernter();
-    Schreiter ernter2 = new Schreiter();
-    Radernter ernter3 = new Radernter();
-    Schreiter ernter4 = new Schreiter();
-    Radernter ernter5 = new Radernter();
-    Schreiter ernter6 = new Schreiter();
-    Radernter ernter7 = new Radernter();
-    Schreiter ernter8 = new Schreiter();
-    Radernter ernter9 = new Radernter();
-    Schreiter ernter10 = new Schreiter();
-    Radernter ernter11 = new Radernter();
-
-
-    Forstbetrieb forstbetrieb21 = new Forstbetrieb("forstbetrieb21");
-    Forstbetrieb forstbetrieb22 = new Forstbetrieb("forstbetrieb22");
-    Forstbetrieb forstbetrieb23 = new Forstbetrieb("forstbetrieb23");
-
-    Forstbetrieb forstbetrieb31 = new Forstbetrieb("forstbetrieb31");
-    Forstbetrieb forstbetrieb32 = new Forstbetrieb("forstbetrieb32");
-    Forstbetrieb forstbetrieb33 = new Forstbetrieb("forstbetrieb33");
-
-    Radernter ernter1 = new Radernter();
-    Schreiter ernter2 = new Schreiter();
-    Radernter ernter3 = new Radernter();
-    Schreiter ernter4 = new Schreiter();
-    Radernter ernter5 = new Radernter();
-    Schreiter ernter6 = new Schreiter();
-    Radernter ernter7 = new Radernter();
-    Schreiter ernter8 = new Schreiter();
-    Radernter ernter9 = new Radernter();
-    Schreiter ernter10 = new Schreiter();
-    Radernter ernter11 = new Radernter();
-
+    Radernter    ernter1        = new Radernter();
+    Schreiter    ernter2        = new Schreiter();
+    Radernter    ernter3        = new Radernter();
+    Schreiter    ernter4        = new Schreiter();
+    Radernter    ernter5        = new Radernter();
+    Schreiter    ernter6        = new Schreiter();
+    Radernter    ernter7        = new Radernter();
+    Schreiter    ernter8        = new Schreiter();
+    Radernter    ernter9        = new Radernter();
+    Schreiter    ernter10       = new Schreiter();
+    Radernter    ernter11       = new Radernter();
 
     public static void main(String[] args) {
         Test test = new Test();
@@ -75,29 +46,33 @@ public class Test {
         gemeinde1.addBetrieb();
         gemeinde1.deleteBetrieb("forstbetrieb12");
 
-        if ((!gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb11"))) || (gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb12"))) || (!gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb13")))) {
+        if ((!gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb11")))
+            || (gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb12")))
+            || (!gemeinde1.containsBetrieb(gemeinde1.getBetriebByName("forstbetrieb13")))) {
             System.out.println("Fehler beim Einfuegen/ Entfernen von Forstbetrieben. ");
             return false;
         }
-
 
         gemeinde2.addBetrieb();
         gemeinde2.addBetrieb();
         gemeinde2.addBetrieb();
         gemeinde2.deleteBetrieb("forstbetrieb22");
 
-        if ((!gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb21"))) || (gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb22"))) || (!gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb23")))) {
+        if ((!gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb21")))
+            || (gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb22")))
+            || (!gemeinde2.containsBetrieb(gemeinde2.getBetriebByName("forstbetrieb23")))) {
             System.out.println("Fehler beim Einfuegen/ Entfernen von Forstbetrieben. ");
             return false;
         }
-
 
         gemeinde3.addBetrieb();
         gemeinde3.addBetrieb();
         gemeinde3.addBetrieb("forstbetrieb33");
         gemeinde3.deleteBetrieb("forstbetrieb32");
 
-        if ((!gemeinde3.containsBetrieb(gemeinde3.getBetriebByName("forstbetrieb31"))) || (gemeinde3.containsBetrieb(gemeinde3.getBetriebByName("forstbetrieb32"))) || (!gemeinde3.containsBetrieb(gemeinde3.getBetriebByName("forstbetrieb33")))) {
+        if ((!gemeinde3.containsBetrieb(gemeinde3.getBetriebByName("forstbetrieb31")))
+            || (gemeinde3.containsBetrieb(gemeinde3.getBetriebByName("forstbetrieb32")))
+            || (!gemeinde3.containsBetrieb(gemeinde3.getBetriebByName("forstbetrieb33")))) {
             System.out.println("Fehler beim Einfuegen/ Entfernen von Forstbetrieben. ");
             return false;
         }
@@ -106,7 +81,8 @@ public class Test {
 
         ernter1.setArbeitskopf(new Schneidearbeitskopf(20.5));
         gemeinde1.getBetriebByName("forstbetrieb11").addHolzvollernter(ernter1); // TODO anpassen
-        gemeinde1.getBetriebByName("forstbetrieb11").changeInformationOfHolzvollernter(1, 24, 10.0, new Schneidearbeitskopf(100.0));
+        gemeinde1.getBetriebByName("forstbetrieb11").changeInformationOfHolzvollernter(1, 24, 10.0,
+                new Schneidearbeitskopf(100.0));
 
         gemeinde1.getBetriebByName("forstbetrieb13").addHolzvollernter(ernter2);
         gemeinde1.getBetriebByName("forstbetrieb13").changeInformationOfHolzvollernter(2, 32, 15,
@@ -125,15 +101,18 @@ public class Test {
                 new Schneidearbeitskopf(125.0));
         gemeinde1.getBetriebByName("forstbetrieb13").deleteHolzvollernter(2);
 
-        if ((!gemeinde1.getBetriebByName("forstbetrieb11").containsHolzvollernter(ernter1)) || (gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter2)) ||
-                (!gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter3)) || (!gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter4)) ||
-                (!gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter5))) {
+        if ((!gemeinde1.getBetriebByName("forstbetrieb11").containsHolzvollernter(ernter1))
+            || (gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter2))
+            || (!gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter3))
+            || (!gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter4))
+            || (!gemeinde1.getBetriebByName("forstbetrieb13").containsHolzvollernter(ernter5))) {
             System.out.println("Fehler beim Einfuegen/ Entfernen von Holzvollerntern. ");
             return false;
         }
 
-        if (!(ernter1.getName().equals(1)) || (!(ernter1.getBetriebsstunden() == 24)) || (!((double) ernter1.getDistanceMoved()==10.0)) ||
-                (!(ernter1.getArbeitskopf() instanceof Schneidearbeitskopf))) {
+        if (!(ernter1.getName().equals(1)) || (!(ernter1.getBetriebsstunden() == 24))
+            || (!((double) ernter1.getDistanceMoved() == 10.0))
+            || (!(ernter1.getArbeitskopf() instanceof Schneidearbeitskopf))) {
             return false;
         }
 
@@ -150,8 +129,9 @@ public class Test {
                 new HackschnitzelArbeitskopf(10));
         gemeinde2.getBetriebByName("forstbetrieb23").deleteHolzvollernter(7);
 
-        if ((!gemeinde2.getBetriebByName("forstbetrieb21").containsHolzvollernter(ernter6)) || (gemeinde2.getBetriebByName("forstbetrieb23").containsHolzvollernter(ernter7)) ||
-                (!gemeinde2.getBetriebByName("forstbetrieb23").containsHolzvollernter(ernter8))) {
+        if ((!gemeinde2.getBetriebByName("forstbetrieb21").containsHolzvollernter(ernter6))
+            || (gemeinde2.getBetriebByName("forstbetrieb23").containsHolzvollernter(ernter7))
+            || (!gemeinde2.getBetriebByName("forstbetrieb23").containsHolzvollernter(ernter8))) {
             System.out.println("Fehler beim Einfuegen/ Entfernen von Holzvollerntern. ");
             return false;
         }
@@ -169,8 +149,9 @@ public class Test {
                 new HackschnitzelArbeitskopf(10));
         gemeinde3.getBetriebByName("forstbetrieb33").deleteHolzvollernter(10);
 
-        if ((!gemeinde3.getBetriebByName("forstbetrieb31").containsHolzvollernter(ernter9)) || (gemeinde3.getBetriebByName("forstbetrieb33").containsHolzvollernter(ernter10)) ||
-                (!gemeinde3.getBetriebByName("forstbetrieb33").containsHolzvollernter(ernter11))) {
+        if ((!gemeinde3.getBetriebByName("forstbetrieb31").containsHolzvollernter(ernter9))
+            || (gemeinde3.getBetriebByName("forstbetrieb33").containsHolzvollernter(ernter10))
+            || (!gemeinde3.getBetriebByName("forstbetrieb33").containsHolzvollernter(ernter11))) {
             System.out.println("Fehler beim Einfuegen/ Entfernen von Holzvollerntern. ");
             return false;
         }
@@ -228,16 +209,17 @@ public class Test {
             ex.printStackTrace();
             return false;
         }
-        if(l.contains(new ListableNumber(5))){
+        if (l.contains(new ListableNumber(5))) {
             return false;
         }
-        
+
         // elemente einfuegen
-        
+
         return false;
     }
-public boolean testStatistiken(){
 
-}
+    public boolean testStatistiken() {
+        return false;
+    }
 
 }
