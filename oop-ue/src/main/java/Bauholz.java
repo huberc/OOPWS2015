ac74fb2cbe0c0856547bc4eaaad137c7e6032533
inesri <rieder.ines@gmail.com>
2015-11-29 18:44:59 +0100

    bauholz und dachstuhl kommentiert

--- oop-ue/src/main/java/Bauholz.java
+++ oop-ue/src/main/java/Bauholz.java
@@ -1,31 +1,34 @@
 
-
 public abstract class Bauholz {
 
+    /* Laenge und Preis des Bauholzes */
     private int laenge;
     private int preis;
 
+    /**
+     * Erstellen eines neuen Bauholzes
+     * VB: laenge und preis sind nicht null
+     * NB: ein Bauholz mit den uebergebenem Parametern wurde erzeugt
+     * @param laenge    Lange des Bauholzes
+     * @param preis     Preis des Bauholzes
+     */
     public Bauholz (int laenge, int preis) {
         this.laenge = laenge;
         this.preis = preis;
     }
 
+    /**
+     * VB: bauholzLager ist nicht null
+     * NB: dieses Bauholz wurde dem bauholzLager hinzugefuegt
+     * @param bauholzLager  Bauholzlager wo dieses Bauholz hinzugeuegt werden soll
+     */
     abstract void add(BauholzLager bauholzLager);
     
     public int getLaenge() {
         return laenge;
     }
-    
-    public void setLaenge(int laenge) {
-        this.laenge = laenge;
-    }
    
     public int getPreis() {
         return preis;
     }
-    
-    public void setPreis(int preis) {
-        this.preis = preis;
-    }
-    
 }
