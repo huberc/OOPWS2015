eeee72cabf96f8ff85681ca2cfb5fda068db7baf
Michael Langowski <e1426581@student.tuwien.ac.at>
2015-11-29 14:57:39 +0100

    created stuuuuuuff

---
+++ oop-ue/src/main/java/BauholzLager.java
@@ -0,0 +1,25 @@
+
+
+public class BauholzLager {
+
+    public void add(){
+        
+    }
+    
+    public Bauholz get(){
+        return null;
+    }
+    
+    public void list(){
+        
+    }
+    
+    public void list(Dachstuhl d){
+        
+    }
+    
+    public void price(Dachstuhl d){
+        
+    }
+    
+}
