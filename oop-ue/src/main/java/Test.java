6c6511701d2cf02c37b2be09cd9de85c28c9941a
inesri <rieder.ines@gmail.com>
2015-11-29 17:40:54 +0100

    blablablabla

--- oop-ue/src/main/java/Test.java
+++ oop-ue/src/main/java/Test.java
@@ -1,15 +1,34 @@
 /**
- * Arbeitsaufteilung:
- *  Christoph Huber:
- *  Michael Langowksi:
- *  Ines Rieder:
+ * Arbeitsaufteilung: Design und test wurde gemeinsam entwickelt und geschrieben
+ *  Christoph Huber: add
+ *  Michael Langowksi: get
+ *  Ines Rieder: list, price
  */
 
 public class Test {
 
     public static void main(String[] args) {
         Test test = new Test();
+        System.out.println(test.keinSaegerauh());
 
+    }
+
+    private boolean keinSaegerauh() {
+        BauholzLager lager = new BauholzLager();
+        Bauholz bauholz1 = new RundholzGehobelt(100, 20000, 10);
+        lager.add(bauholz1);
+        Bauholz bauholz2 = new KantholzGehobelt(100, 30000, 10, 10);
+        lager.add(bauholz2);
+        Dachstuhl holzNutzbauDachstuhl = new HolzNutzbauDachstuhl();
+        Dachstuhl steinNutzbauDachstuhl = new SteinNutzbauDachstuhl();
+        Bauholz tmpHolz = lager.get(holzNutzbauDachstuhl);
+        Bauholz tmpStein = lager.get(steinNutzbauDachstuhl);
+
+        if ((tmpHolz == bauholz1) && (tmpStein == bauholz2)) {
+            return true;
+        }
+
+        return false;
     }
 
 }
